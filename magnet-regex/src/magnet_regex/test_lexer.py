d2ab55598958763306007512c3cd4c6a146b3392
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-02-17 12:44:47 +0100

    Demo pattern test and fixed previous test

--- magnet-regex/src/magnet_regex/test_lexer.py
+++ magnet-regex/src/magnet_regex/test_lexer.py
@@ -3,14 +3,53 @@ from magnet_regex.lexer import Lexer, Token, TokenType
 
 class TestLexer(unittest.TestCase):
     def test_tokenise_backref(self):
-        lexer = Lexer(r"This: \1\2 is a backref \123 test")
+        lexer = Lexer(r"Backref \1\2 test")
         tokens = lexer.tokenize()
 
         self.assertEqual(
             tokens,
             [
-                Token(TokenType.BACKREF, r"\1", 6),
-                Token(TokenType.BACKREF, r"\2", 8),
-                Token(TokenType.BACKREF, r"\123", 24),
+                Token(TokenType.CHAR, r"B", 0),
+                Token(TokenType.CHAR, r"a", 1),
+                Token(TokenType.CHAR, r"c", 2),
+                Token(TokenType.CHAR, r"k", 3),
+                Token(TokenType.CHAR, r"r", 4),
+                Token(TokenType.CHAR, r"e", 5),
+                Token(TokenType.CHAR, r"f", 6),
+                Token(TokenType.CHAR, r" ", 7),
+                Token(TokenType.BACKREF, r"\1", 8),
+                Token(TokenType.BACKREF, r"\2", 10),
+                Token(TokenType.CHAR, r" ", 12),
+                Token(TokenType.CHAR, r"t", 13),
+                Token(TokenType.CHAR, r"e", 14),
+                Token(TokenType.CHAR, r"s", 15),
+                Token(TokenType.CHAR, r"t", 16),
+                Token(TokenType.EOF, None, 17),
+            ]
+        )
+
+    def test_demo_pattern(self):
+        lexer = Lexer(r"a\d{1}(?:foo|bar)?")
+        tokens = lexer.tokenize()
+
+        self.assertEqual(
+            tokens,
+            [
+                Token(TokenType.CHAR, r"a", 0),
+                Token(TokenType.DIGIT, r"\d", 1),
+                Token(TokenType.LBRACE, r"{", 3),
+                Token(TokenType.CHAR, r"1", 4),
+                Token(TokenType.RBRACE, r"}", 5),
+                Token(TokenType.NON_CAPTURING, r"(?:", 6),
+                Token(TokenType.CHAR, r"f", 9),
+                Token(TokenType.CHAR, r"o", 10),
+                Token(TokenType.CHAR, r"o", 11),
+                Token(TokenType.PIPE, r"|", 12),
+                Token(TokenType.CHAR, r"b", 13),
+                Token(TokenType.CHAR, r"a", 14),
+                Token(TokenType.CHAR, r"r", 15),
+                Token(TokenType.RPAREN, r")", 16),
+                Token(TokenType.QUESTION, r"?", 17),
+                Token(TokenType.EOF, None, 18),
             ]
         )
