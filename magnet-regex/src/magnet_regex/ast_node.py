4a588b61cce07d0942da6fa260b4866f00cc337e
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-02-18 11:14:37 +0100

    Quantifier node

--- magnet-regex/src/magnet_regex/ast_node.py
+++ magnet-regex/src/magnet_regex/ast_node.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Set
+from typing import Optional, Set
 
 
 @dataclass
@@ -40,3 +40,35 @@ class PredefinedClassNode(ASTNode):
 
     def __repr__(self):
         return f"PredefinedClass(\\{self.class_type})"
+
+
+@dataclass
+class QuantifierNode(ASTNode):
+    """Quantifies a given node. Quantifiers can be: *, +, ?, {n}, {n,m}, {n,}. See lexer.py for
+    more information"""
+    child: ASTNode 
+    min_count: int
+    max_count: Optional[int]
+    # Greedy mode (by default), matches as many characters as possible. For example, by default
+    # the regex -> ".*" matches everything between 2 quotes, as such, for the string ->
+    # "hello" some room "world" will be matched from the first quote (position 0) up to the last
+    # quote (after world).
+    # The lazy match will have 2 matches, one for each group of characters between quotes.
+    # False for lazy qunatifiers: (*?, +?, ??, {n,m}?)
+    greedy: bool = True
+
+    def __repr__(self):
+        q = None
+        if self.min_count == 0 and self.max_count == 1:
+            q = "?"
+        elif self.min_count == 0 and self.max_count is None:
+            q = "*"
+        elif self.min_count == 1 and self.max_count is None:
+            q = "+"
+        else:
+            q = f"{{{self.min_count}, {self.max_count}}}"
+        
+        if not self.greedy:
+            q += "?"
+        
+        return f"Quantifier({self.child} {q})"
