498def7d6205d75b976d87f67fe9a8ac1c7e6bf5
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2026-02-18 10:51:37 +0100

    Class nodes

--- magnet-regex/src/magnet_regex/ast_node.py
+++ magnet-regex/src/magnet_regex/ast_node.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Set
 
 
 @dataclass
@@ -11,3 +12,31 @@ class CharNode(ASTNode):
 
     def __repr__(self):
         return f"Char({self.char!r})"
+
+@dataclass
+class DotNode(ASTNode): 
+    def __repr__(self):
+        return "Dor(.)"
+
+@dataclass
+class CharClassNode(ASTNode): 
+    """Characters with regex brackets `[...]`"""
+    chars: Set[str]
+    negated: bool = False # True if the class contains a caret: [^m]
+
+    def __repr__(self):
+        prefix = "^" if self.negated else ""
+        chars_str = "".join(sorted(self.chars)[:10])
+        if len(self.chars) > 10:
+            chars_str += "..."
+        return f"CharClass([{prefix}{chars_str}])"
+
+
+@dataclass
+class PredefinedClassNode(ASTNode):
+    """A class node with a known key characteristic: \d, \D, \w, \W, etc"""
+    # TODO: Shoud this be a enum?
+    class_type: str
+
+    def __repr__(self):
+        return f"PredefinedClass(\\{self.class_type})"
